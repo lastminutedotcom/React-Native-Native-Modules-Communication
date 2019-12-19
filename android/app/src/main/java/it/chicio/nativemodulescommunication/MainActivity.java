e83fb33775804b2a8fc70a32c3301620d096c3c9
Fabrizio Duroni <fabrizio.duroni@gmail.com>
2019-12-19 09:01:43 +0100

    Initial commit :tada:

---
+++ android/app/src/main/java/it/chicio/nativemodulescommunication/MainActivity.java
@@ -0,0 +1,19 @@
+package it.chicio.nativemodulescommunication;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void showReactNativeModal(View view) {
+        startActivity(new Intent(this, ReactNativeModalActivity.class));
+    }
+}
